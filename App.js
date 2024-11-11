4ab9fd51f1b9fef201c3171d45106ca6c747910d
Bart <brayson@live.com>
2024-11-11 10:58:38 +0000

    fixed weather card and rendered it

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 // Import data and WeatherCard here
+import WeatherCard from './components/WeatherCard';
+import cities from './data'
 
 
 function App() {
@@ -9,6 +11,9 @@ function App() {
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
                 {/* Render components here */}
+                {cities.map((city) => (
+                    <WeatherCard key={city.city} city={city}/>
+                ))}
                 
 
 

d2fd35ebe6b75e90e19eaad26bd7811c9246add5
Bart <brayson@live.com>
2024-11-13 10:51:07 +0000

    added search funtionality

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import {React, useState} from 'react';
 import WeatherCard from './components/WeatherCard';
 import Location from './components/Location';
 import cities from './data'
+import Form from './components/Form';
 
 
 function App() {
     const [location, setLocation] = useState("London")
     return (
         <>
+            <Form location={location} setLocation={setLocation}/>
             <Location location = {location} setLocation={setLocation} data={cities}/>
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
